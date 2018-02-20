544438a6b415879d570ba78988e0ce91f2357e75
reute <j.rtl@gmx.de>
2018-02-20 19:46:06 +0100

    First commit

---
+++ five/Models/Order.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace five.Models
+{
+    public class Order
+    {      
+        public DateTime Datum { get; set; }  
+
+        public Kategorie Kategorie { get; set; }  
+
+        public string Detail { get; set; }  
+      
+        public Referent Referent { get; set; }  
+       
+        public int Menge { get; set; } 
+   
+        public decimal Einzelpreis { get; set; }        
+
+        public decimal Gesamtpreis { get; set; } 
+
+        //public List<Kategorie> KatList { get; set; } = new List<Kategorie> { Kategorie.Bremsbelag, Kategorie.Kurbel, Kategorie.Schlauch };
+    }
+
+    public enum Kategorie
+    {
+        Schlauch,
+        Kette,        
+        Bremsbelag,
+        Zug,
+        Reifen,      
+        Kasette,
+        Klingel,
+        Griffe,
+        Licht,
+        Birne,
+        Kurbel,
+        Lenkerband,
+        Verschieden
+    }
+
+    public enum Referent
+    {
+        Jr,
+        Jc,
+        Cf
+    }
+}
+
---
+++ five/Services/OrderDataService.cs
@@ -0,0 +1,43 @@
+﻿using five.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace five.Services
+{
+    public static class OrderDataService
+    {
+        private static IEnumerable<Order> AllOrders()
+        {            
+            var data = new ObservableCollection<Order>
+            {
+                new Order
+                {
+                    Datum = new DateTime(2017, 05, 24),
+                    Kategorie = Kategorie.Bremsbelag,
+                    Detail = "V-Brake",
+                    Referent = Referent.Jr,
+                    Menge = 2,
+                    Einzelpreis = 2,
+                    Gesamtpreis = 4
+                },
+                new Order
+                {
+                    Datum = new DateTime(2017, 05, 24),
+                    Kategorie = Kategorie.Reifen,
+                    Detail = "Schwalbe Roadcruiser",
+                    Referent = Referent.Jc,
+                    Menge = 1,
+                    Einzelpreis = 10,
+                    Gesamtpreis = 10
+                },                
+            };
+            return data;
+        }
+
+        public static ObservableCollection<Order> GetOrderData()
+        {
+            return new ObservableCollection<Order>(AllOrders());
+        }
+    }
+}
